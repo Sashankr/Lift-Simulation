39cd646497fb107794d1a2aa7370fedc5424d875
Sashankr <sashankr97@gmail.com>
2022-11-28 12:29:28 +0530

    added count validation to proceed to next step

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,17 @@
+const validateCount = (count) => {
+  if (count > 10 || count < 0) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
 const intialUserInputsHandler = (event) => {
   event.preventDefault();
   const floorCount = Number(document.querySelector('[name = "floors"]').value);
   const liftCount = Number(document.querySelector('[name = "lifts"]').value);
-  console.log(floorCount, liftCount);
+
+  if (validateCount(floorCount) && validateCount(liftCount)) {
+    document.querySelector(".lift-simulator__home").classList.add("hide");
+  }
 };
