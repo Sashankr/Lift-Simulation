52b5b997cb556da1dba1d415cfb3ecd3d98d20fa
Sashankr <sashankr97@gmail.com>
2022-11-28 12:22:39 +0530

    captured number of lifts and floors

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,6 @@
 const intialUserInputsHandler = (event) => {
   event.preventDefault();
+  const floorCount = Number(document.querySelector('[name = "floors"]').value);
+  const liftCount = Number(document.querySelector('[name = "lifts"]').value);
+  console.log(floorCount, liftCount);
 };
