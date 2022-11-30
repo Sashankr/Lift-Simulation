7b5769c1f9d88a706148debf3ff03e6e22c06228
Sashank Rampalli <sashankr97@gmail.com>
2022-12-01 00:14:26 +0530

    completed moving lift when we click on up button

--- src/js/main.js
+++ src/js/main.js
@@ -26,12 +26,16 @@ const floorUI = (floorCount) => {
       <div class="floor-button-container">
       <button class="floor-up-button" data-up-button="${floorCount}" >UP</button>
       ${
-        floorCount !== 1
+        floorCount !== 0
           ? `<button class="floor-down-button" data-down-button="${floorCount}">DOWN</button>`
           : ""
       }
       </div>
-      <h2 class="floor-title">Floor ${floorCount}</h2>
+      ${
+        floorCount === 0
+          ? '<h2 class="floor-title">Ground Floor</h2>'
+          : `<h2 class="floor-title">Floor ${floorCount}</h2>`
+      }
     </div>
   `;
   return {
@@ -51,15 +55,36 @@ const liftUi = (liftCount) => {
 
 const buildInteractiveUI = (liftCount, floorCount) => {
   const liftSimulator = document.querySelector(".lift-simulator");
-  for (let i = floorCount; i > 0; i--) {
+  for (let i = floorCount; i >= 0; i--) {
     const { floorContainer } = floorUI(i);
     appendUIElement(liftSimulator, floorContainer);
   }
-  const firstFloor = document.querySelector('[data-floor="1"]');
+  const firstFloor = document.querySelector('[data-floor="0"]');
 
   for (let j = 1; j <= liftCount; j++) {
     const liftContainer = liftUi(j);
     appendUIElement(firstFloor, liftContainer);
+  }
+};
+
+const moveLift = (event, direction) => {
+  const firstLift = document.querySelector('[data-lift="1"]');
+  const offsetValue = 200;
+
+  if (direction === "up") {
+    const upButtonId = Number(event.target.getAttribute("data-up-button"));
+    if (upButtonId === 0) {
+      firstLift.style.transform = "translateY(0px)";
+    } else {
+      firstLift.style.transform = `translateY(-${upButtonId * offsetValue}px)`;
+    }
+  } else {
+    const downButtonId = Number(event.target.getAttribute("data-down-button"));
+    // if (downButtonId === 0) {
+    //   firstLift.style.transform = "translateY(0px)";
+    // } else {
+    //   firstLift.style.transform = `translateY(${downButtonId * offsetValue}px)`;
+    // }
   }
 };
 
@@ -71,9 +96,15 @@ const intialUserInputsHandler = (event) => {
   if (validateCount(floorCount) && validateCount(liftCount)) {
     document.querySelector(".lift-simulator__home").classList.add("hide");
     buildInteractiveUI(liftCount, floorCount);
+    const floorUpButtons = document.querySelectorAll(".floor-up-button");
+    const floorDownButtons = document.querySelectorAll(".floor-down-button");
+
+    Array.from(floorUpButtons).forEach((button) =>
+      button.addEventListener("click", (event) => moveLift(event, "up"))
+    );
+
+    Array.from(floorDownButtons).forEach((button) =>
+      button.addEventListener("click", (event) => moveLift(event, "down"))
+    );
   }
 };
-
-const moveLiftUp = () => {};
-
-// document.querySelectorAll('.floor-up-button').
